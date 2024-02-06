b93ce64fad103f9e6b3ed72a3344edca19779408
otris <theliomt@gmail.com>
2024-02-06 15:04:07 -0300

    Add favicon.ico, update next.config.mjs, and modify SVG icons

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/front",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
