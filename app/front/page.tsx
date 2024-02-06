97061f407ddecd1e7f99efae1955049961709105
otris <theliomt@gmail.com>
2024-02-06 15:17:11 -0300

    Refactor page component structure

--- app/front/page.tsx
+++ app/front/page.tsx
@@ -2,17 +2,15 @@
 
 import Hero from "../components/heroSection/hero";
 import Projects from "../components/projectsSection/projects";
-function trackGradient(e: any) {
-  console.log(e);
-}
 
 export default function Home() {
   return (
-    <div
-      onClick={trackGradient}
-      className="w-full h-full flex flex-col items-center justify-center "
-    >
-      <Hero />
-    </div>
+    <>
+      <link rel="icon" href="/favicon.ico" sizes="any" />
+
+      <div className="w-full h-full flex flex-col items-center justify-center ">
+        <Hero />
+      </div>
+    </>
   );
 }
